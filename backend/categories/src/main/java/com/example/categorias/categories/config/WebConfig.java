26a0aa46b6d1ed18e74e5dcef568300f7593d725
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-08-11 16:35:08 -0500

    changes

---
+++ backend/categories/src/main/java/com/example/categorias/categories/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.categorias.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+  
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://18.117.178.149:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+      }
+    };
+  }
+}
+
---
+++ backend/products/src/main/java/com/example/products/products/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.products.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+  
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://18.117.178.149:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+      }
+    };
+  }
+}
+
+git add ../../backend/categories/src/main/java/com/example/categorias/categories/config/WebConfig.java
+git add ../../backend/products/src/main/java/com/example/products/products/config/WebConfig.java
